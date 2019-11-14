73ee63a401f93450901fb4d1868376841aceedc1
Alexandra Iordache <aghecen@amazon.com>
2019-11-14 17:47:31 +0200

    ser/de: add derive macros to structs relevant...
    ...for snapshotting, which can be directly interpreted by serde
    with no additional work.
    Added TODOs for remaining relevant structs (& unions) that need a
    bit of handicraft.
    Added ser/deser unit tests using serde_json as serializer.
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- src/x86/mod.rs
+++ src/x86/mod.rs
@@ -28,3 +28,108 @@ pub mod bindings {
     #[cfg(feature = "fam-wrappers")]
     pub use super::fam_wrappers::*;
 }
+
+#[cfg(all(test, feature = "with-serde"))]
+mod tests {
+    extern crate serde_json;
+
+    use super::bindings::*;
+
+    #[test]
+    fn test_ser_deser() {
+        {
+            // Test kvm_regs ser/deser.
+            let val = kvm_regs::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_regs>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_segment ser/deser.
+            let val = kvm_segment::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_segment>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_dtable ser/deser.
+            let val = kvm_dtable::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_dtable>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_sregs ser/deser.
+            let val = kvm_sregs::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_sregs>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_pit_state2 ser/deser.
+            // Also covers kvm_pit_channel_state.
+            let val = kvm_pit_state2::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_pit_state2>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_vcpu_events ser/deser.
+            // Also covers:
+            // * kvm_vcpu_events__bindgen_ty_1
+            // * kvm_vcpu_events__bindgen_ty_2
+            // * kvm_vcpu_events__bindgen_ty_3
+            // * kvm_vcpu_events__bindgen_ty_4
+            let val = kvm_vcpu_events::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_vcpu_events>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_debugregs ser/deser.
+            let val = kvm_debugregs::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_debugregs>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_xcrs ser/deser.
+            // Also covers kvm_xcr.
+            let val = kvm_xcrs::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_xcrs>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_mp_state ser/deser.
+            let val = kvm_mp_state::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_mp_state>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_clock_data ser/deser.
+            let val = kvm_clock_data::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_clock_data>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+
+        {
+            // Test kvm_regs ser/deser.
+            let val = kvm_regs::default();
+            let val_ser = serde_json::to_string(&val).unwrap();
+            let val_deser = serde_json::from_str::<kvm_regs>(val_ser.as_str()).unwrap();
+            assert_eq!(val, val_deser);
+        }
+    }
+}
