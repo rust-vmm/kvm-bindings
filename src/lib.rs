d57aa297da694b866e6b124f03d36fe18e8c8ed9
Alexandra Iordache <aghecen@amazon.com>
2019-12-19 16:09:21 +0200

    add global version variable
    Composed of architecture, kernel version and crate version
    (all strings). Up to the consumer to decide how to handle it.
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,41 @@ extern crate serde_derive;
 mod x86;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub use self::x86::bindings::*;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub use self::x86::Version;
 
 #[cfg(target_arch = "aarch")]
 mod arm;
 #[cfg(target_arch = "aarch")]
 pub use self::arm::bindings::*;
+#[cfg(target_arch = "aarch")]
+pub use self::arm::Version;
 
 #[cfg(target_arch = "aarch64")]
 mod arm64;
 #[cfg(target_arch = "aarch64")]
 pub use self::arm64::bindings::*;
+#[cfg(target_arch = "aarch64")]
+pub use self::arm64::Version;
+
+#[cfg(all(test, feature = "with-serde"))]
+mod tests {
+    extern crate serde_json;
+
+    use super::Version;
+
+    #[test]
+    fn test_version_ser_deser() {
+        let ver = Version {
+            arch: "x86_64",
+            kernel_ver: "v4.20.0",
+            crate_ver: "v0.2.0",
+        };
+        let ver_str = "{\"arch\":\"x86_64\",\"kernel_ver\":\"v4.20.0\",\"crate_ver\":\"v0.2.0\"}";
+        let ver_ser = serde_json::to_string(&ver).unwrap();
+        assert_eq!(ver_ser, ver_str.to_string());
+
+        let ver_deser = serde_json::from_str::<Version>(ver_str).unwrap();
+        assert_eq!(ver, ver_deser);
+    }
+}
