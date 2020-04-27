f89e704e2f6643192ced96505e1d05c32c85b6be
Adrian Catangiu <acatan@amazon.com>
2020-04-27 13:56:13 +0300

    x86: add versioning for x86_64 state structs
    Signed-off-by: Adrian Catangiu <acatan@amazon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@
 #[cfg(feature = "fam-wrappers")]
 extern crate vmm_sys_util;
 
+extern crate versionize;
+extern crate versionize_derive;
+
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod x86;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
